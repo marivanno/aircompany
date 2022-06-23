9bc4bb43bbbc290eb53281a149a23610c00f0c34
Ivan Shadrin <67239357+marivanno@users.noreply.github.com>
2022-06-23 17:35:51 +0300

    delete extra file Experimental Types.js

--- JS/models/ExperimentalTypes.js
+++
@@ -1,7 +0,0 @@
-export default {
-    LIFTING_BODY: 'Lifting body',
-    HYPER_SONIC: 'Hyper sonic',
-    HIGHT_ALTITUDE: 'High altitude',
-    VERTICAL_TAKE_OFF_LANDING: 'Vertical Take-Off and Landing'
-};
-
